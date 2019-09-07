2b9c9629359b5870d9f70a503255090ef4e31349
Andrew Kirwin <andrew.kirwin@gmail.com>
2019-09-07 11:51:17 +0100

    test

---
+++ tests/integration/components/tooltipper/is-showing-is-hiding-class-test.js
@@ -0,0 +1,44 @@
+import { module, test } from 'qunit';
+import setupTooltipperTest from './setup';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('tooltipper', function(hooks) {
+  setupTooltipperTest(hooks);
+
+  test('is-showing/is-hiding class', async function(assert) {
+    assert.expect(4);
+
+    this.set('showTooltip', true);
+
+    await render(hbs`
+      <Tooltipper
+        @showTooltip={{this.showTooltip}}
+        @tooltip={{component "foo-tooltip"}} />
+    `);
+
+    assert
+      .dom('.tooltip')
+      .hasClass(
+        'is-showing',
+        'precondition: is showing class is added to trigger animations'
+      );
+
+    assert
+      .dom('.tooltip')
+      .doesNotHaveClass('is-hiding', 'precondition: is not hiding');
+
+    this.set('showTooltip', false);
+
+    assert
+      .dom('.tooltip')
+      .doesNotHaveClass(
+        'is-showing',
+        'is showing class is removed to trigger animations'
+      );
+
+    assert
+      .dom('.tooltip')
+      .hasClass('is-hiding', 'is hiding class is added to trigger animations');
+  });
+});
---
+++ tests/integration/components/tooltipper/onload-action-test.js
@@ -0,0 +1,40 @@
+import { module, test } from 'qunit';
+import setupTooltipperTest from './setup';
+import { render, settled } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { defer } from 'rsvp';
+
+module('tooltipper', function(hooks) {
+  setupTooltipperTest(hooks);
+
+  test('onLoad action', async function(assert) {
+    assert.expect(3);
+
+    const deferred = defer();
+
+    this.loadTooltip = () => deferred.promise;
+
+    await render(hbs`
+      <Tooltipper
+        @showTooltip={{true}}
+        @onLoad={{this.loadTooltip}}
+        @tooltip={{component "foo-tooltip"}} />
+    `);
+
+    assert.dom('.tooltip').doesNotExist('not rendered tooltip yet');
+
+    deferred.resolve({ name: 'World' });
+
+    await settled();
+
+    assert
+      .dom('.tooltip')
+      .exists(
+        'renders the tooltip when the tooltipper has loaded the necessary data'
+      );
+
+    assert
+      .dom('.tooltip')
+      .hasText('Hello World', 'the loaded data is passed to the tooltip');
+  });
+});
