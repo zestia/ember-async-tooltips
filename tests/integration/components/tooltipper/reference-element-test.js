f695c4e78b79689b4ece996b338cc3aaa1d65dac
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-09-09 15:31:39 +0100

    test

---
+++ tests/integration/components/tooltipper/reference-element-test.js
@@ -0,0 +1,40 @@
+import { module, test } from 'qunit';
+import setupTooltipperTest from './setup';
+import { render, find, triggerEvent } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('tooltipper', function(hooks) {
+  setupTooltipperTest(hooks);
+
+  test('reference element', async function(assert) {
+    assert.expect(2);
+
+    await render(hbs`
+      {{#if this.referenceElement}}
+        <Tooltipper
+          @tooltip={{component "foo-tooltip"}}
+          @referenceElement={{this.referenceElement}} />
+      {{/if}}
+
+      <div class="reference-element"></div>
+    `);
+
+    this.set('referenceElement', find('.reference-element'));
+
+    await triggerEvent('.reference-element', 'mouseenter');
+
+    assert
+      .dom('.tooltip')
+      .exists('renders tooltip when mousing over the reference element');
+
+    await triggerEvent('.reference-element', 'mouseleave');
+
+    await triggerEvent('.tooltip', 'animationend');
+
+    assert
+      .dom('.tooltip')
+      .doesNotExist(
+        'tooltip is destroyed when mousing out of the reference element'
+      );
+  });
+});
