4fbe8020af6b67a4927255e9e1a9f598cad942ba
Andrew Kirwin <andrew.kirwin@zestia.com>
2020-02-13 13:40:23 +0000

    run ember-cli-update

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,16 +1,12 @@
-/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
-
 import Application from '@ember/application';
-import Resolver from './resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const app = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
-loadInitializers(app, config.modulePrefix);
-
-export default app;
+loadInitializers(App, config.modulePrefix);
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,15 +1,13 @@
-import Router from '@ember/routing/router';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const DummyRouter = Router.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
-DummyRouter.map(function() {
+Router.map(function() {
   this.route('simple');
   this.route('reference');
   this.route('position');
 });
-
-export default DummyRouter;
