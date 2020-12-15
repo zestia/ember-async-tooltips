93168c9169a7e2a1e15ee37f1325a1a98922b551
Andrew Kirwin <andrew.kirwin@gmail.com>
2020-12-15 21:24:16 +0000

    move to integration test

---
+++ tests/integration/components/tooltipper/hide-all-test.js
@@ -0,0 +1,36 @@
+import { module, test } from 'qunit';
+import setupTooltipperTest from './setup';
+import { render, settled } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('tooltipper', function (hooks) {
+  setupTooltipperTest(hooks);
+
+  test('hide all', async function (assert) {
+    assert.expect(2);
+
+    await render(hbs`
+      <Tooltipper
+        @showTooltip={{true}}
+        @tooltip={{component "tooltip"}}
+      />
+
+      <Tooltipper
+        @showTooltip={{true}}
+        @tooltip={{component "tooltip"}}
+      />
+    `);
+
+    assert
+      .dom('.tooltip')
+      .exists({ count: 2 }, 'precondition: two tooltips are rendered');
+
+    await this.owner.lookup('service:tooltip').hideAllTooltips();
+
+    await settled();
+
+    assert
+      .dom('.tooltip')
+      .exists({ count: 0 }, 'tooltips are no longer rendered');
+  });
+});
