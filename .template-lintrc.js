04f84fed3d21e586b8abb02c14467f32ecaab0c0
Andrew Kirwin <github@andrewkirwin.co.uk>
2022-05-17 22:20:57 +0100

    upgrade deps

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  plugins: ['@zestia/template-lint-config']
+  plugins: ['@zestia/template-lint-config'],
+  extends: 'zestia:recommended',
+
+  rules: {
+    'no-capital-arguments': 'off'
+  }
 };
