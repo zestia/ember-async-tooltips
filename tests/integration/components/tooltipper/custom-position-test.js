2fc253e145434abf12395f3a1493fed6abf7f795
Andrew Kirwin <github@andrewkirwin.co.uk>
2022-10-07 09:57:44 +0100

    add missing test cases

--- tests/integration/components/tooltipper/custom-position-test.js
+++ tests/integration/components/tooltipper/custom-position-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
+import hbs from 'htmlbars-inline-precompile';
 import setupTooltipperTest from './setup';
 import { render, triggerEvent, getRootElement } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
 
 module('tooltipper', function (hooks) {
   setupTooltipperTest(hooks);
@@ -40,5 +40,38 @@ module('tooltipper', function (hooks) {
       .hasAttribute('data-position', 'left top', 'custom position is used');
 
     getRootElement().parentNode.classList.remove('full-screen');
+  });
+
+  test('no position', async function (assert) {
+    assert.expect(1);
+
+    await render(hbs`
+      <Tooltipper @Tooltip={{component "tooltip" text="See me"}} />
+    `);
+
+    await triggerEvent('.tooltipper', 'mouseenter');
+
+    assert
+      .dom('.tooltip')
+      .hasAttribute('data-position', 'bottom center', 'default');
+  });
+
+  test('invalid position', async function (assert) {
+    assert.expect(2);
+
+    await render(hbs`
+      <Tooltipper
+        @Tooltip={{component "tooltip" text="See me"}}
+        @position="foo"
+      />
+    `);
+
+    await triggerEvent('.tooltipper', 'mouseenter');
+
+    assert.dom('.tooltip').hasAttribute('data-position', 'foo');
+
+    assert
+      .dom('.tooltip')
+      .hasAttribute('style', 'top: NaNpx; left: NaNpx', 'fails silently');
   });
 });
