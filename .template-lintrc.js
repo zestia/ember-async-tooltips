9f4fa42d0f43b892fd65dd3b1e824b2a2a87a7e7
Andrew Kirwin <github@andrewkirwin.co.uk>
2023-01-26 13:28:50 +0000

    Update comment

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   plugins: ['@zestia/template-lint-config'],
   extends: 'zestia:recommended',
-  // Temporarily disable linting pretty-ness of hbs in js files (but still lint hbs in js).
+  // Temporarily disable linting pretty-ness of hbs in js files
   // Issue: https://discord.com/channels/480462759797063690/666416704418611233/1052203658445598722
   overrides: [
     {
