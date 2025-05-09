072420cbb1427820d9dde6cc75e4b0bc265ea9a7
Andrew Kirwin <github@andrewkirwin.co.uk>
2025-05-09 12:23:47 +0100

    run ember-cli-update

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -7,47 +7,47 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-4.12',
+        name: 'ember-lts-5.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.12.0'
-          }
-        }
+            'ember-source': '~5.8.0',
+          },
+        },
       },
       {
-        name: 'ember-lts-5.4',
+        name: 'ember-lts-5.12',
         npm: {
           devDependencies: {
-            'ember-source': '~5.4.0'
-          }
-        }
+            'ember-source': '~5.12.0',
+          },
+        },
       },
       {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
+            'ember-source': await getChannelURL('release'),
+          },
+        },
       },
       {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
       },
       {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
       },
       embroiderSafe(),
-      embroiderOptimized()
-    ]
+      embroiderOptimized(),
+    ],
   };
 };
